06ac2a0577f6b56799aa594208e0a39a89472cb3
DeepakXD94 <109253936+DeepakXD94@users.noreply.github.com>
2022-08-07 15:05:13 +0530

    SunSpot data manipulation
    A program that manipulates JSON Data on yearly averages of Wolf Sun Spot Data for years 1770-1869,
    #  also known as Box-Jenkins Series E.

---
+++ sunspot-data/sun-spot-data.py
@@ -0,0 +1,17 @@
+# A program that manipulates JSON Data on yearly averages of Wolf Sun Spot Data for years 1770-1869,
+#  also known as Box-Jenkins Series E.
+
+import json
+
+JsonObj = open('Sample-Data-Wolf-Sunspot-Numbers.json') # Creating File  Object.
+JsonData = json.load(JsonObj) # load(FileObj) to load File Object.
+
+raw_list = [] # Defining  a list where the raw data from JSON File will be stored.
+for list in JsonData:
+    raw_list.append(list) # JSON elements are appended to the empty list.
+    
+JsonObj.close()
+
+data_list = raw_list[2][1][1]    # The refined data is now moved to a new list.
+data_list.remove("List" )    # The first element - "List" is removed.
+# print(data_list)
