98132aaf00b0c785d97cc7c0279f3dfb168838fd
DeepakXD94 <109253936+DeepakXD94@users.noreply.github.com>
2022-08-07 15:15:43 +0530

    Updated Code.
    Just used with statement to open and process JSON File.

--- sunspot-data/sun-spot-data.py
+++ sunspot-data/sun-spot-data.py
@@ -3,15 +3,13 @@
 
 import json
 
-JsonObj = open('Sample-Data-Wolf-Sunspot-Numbers.json') # Creating File  Object.
-JsonData = json.load(JsonObj) # load(FileObj) to load File Object.
+with open('Sample-Data-Wolf-Sunspot-Numbers.json') as JsonObj:    # Creating File  Object.
+    JsonData = json.load(JsonObj) # load(FileObj) to load File Object.
 
-raw_list = [] # Defining  a list where the raw data from JSON File will be stored.
+raw_list = []   # Defining  a list where the raw data from JSON File will be stored.
 for list in JsonData:
-    raw_list.append(list) # JSON elements are appended to the empty list.
-    
-JsonObj.close()
+    raw_list.append(list)   # JSON elements are appended to the empty list.
 
 data_list = raw_list[2][1][1]    # The refined data is now moved to a new list.
 data_list.remove("List" )    # The first element - "List" is removed.
-# print(data_list)
+print(data_list)
